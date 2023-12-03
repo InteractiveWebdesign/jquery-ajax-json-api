9ebc2fb306646a0e5896ae7fdbb4a86b034bc876
Ruud <mrwheyters@proton.me>
2023-12-03 08:09:59 +0000

    Created animation for h1 colorchange

---
+++ htdocs/js/change-title.js
@@ -0,0 +1,22 @@
+
+
+/* function that changes the h1 color to blue when the user clicks on it.
+
+$(function () {
+    $('h1').click(function () {
+        // Change the color of the clicked h1 element to blue
+        $(this).css('color', 'blue');
+    });
+});
+*/
+
+
+// ANIMATED function that changes the h1 color to blue when the user clicks on it.
+$(function () {
+    $('h1').click(function () {
+        // Use the animate function to smoothly change the color to blue
+        $(this).animate({
+            color: 'blue'
+        }, 500); // 1000 milliseconds (1 second) for the animation duration
+    });
+});
